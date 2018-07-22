a3ad900eed4b2602582752f095e111e9b225e883
Abraham Tesfy <Abraham@Abrahams-MacBook-Pro.local>
2018-07-22 15:45:15 -0700

    Abstraction

---
+++ src/Parrot.java
@@ -0,0 +1,12 @@
+
+public class Parrot extends Bird{
+    public Parrot(String name) {
+        super(name);
+    }
+
+	@Override
+	public void fly() {
+		// TODO Auto-generated method stub
+		
+	}
+}
